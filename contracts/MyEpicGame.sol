c6f9030faa421ed2fcf3414d2382b95444f7da33
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-12-28 20:20:25 +0530

    feat: create constructor to add possible characters

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,39 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT");
+
+  struct CharacterAttributes {
+    uint characterIndex;
+    string name;
+    string imageURI;
+    uint hp;
+    uint maxHp;
+    uint attackDamage;
+    uint intelligence;
+  }
+
+  CharacterAttributes[] defaultCharacters;
+
+  constructor(
+    string[] memory characterNames,
+    string[] memory characterImageURIs,
+    uint[] memory characterHp,
+    uint[] memory characterAttackDmg,
+    uint[] memory characterIntelligence
+  ) {
+    for(uint i = 0; i < characterNames.length; i+= 1) {
+      defaultCharacters.push(CharacterAttributes({
+        characterIndex: i,
+        name: characterNames[i],
+        imageURI: characterImageURIs[i],
+        hp: characterHp[i],
+        maxHp: characterHp[i],
+        attackDamage: characterAttackDmg[i],
+        intelligence: characterIntelligence[i]
+      }));
+
+      CharacterAttributes memory c = defaultCharacters[i];
+      console.log("Done initializing %s w/ HP %s, Intelligence %s", c.name, c.hp, c.intelligence);
+    }
   }
 }
